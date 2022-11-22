bacf588650a4bc65abb6640306fef908229e484d
Martin Cartledge <mcartledge@opentable.com>
2022-11-22 06:06:33 -0700

    Run PackerSync when new entry is added to plugins.lua

--- nvim/lua/martincartledge/plugins-setup.lua
+++ nvim/lua/martincartledge/plugins-setup.lua
@@ -12,7 +12,7 @@ local packer_bootstrap = ensure_packer() -- true if packer was just installed
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
 
