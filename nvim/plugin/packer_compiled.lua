75d8325259a6276466c6ffa8d8af67225393ed12
Martin Cartledge <mcartledge@opentable.com>
2022-11-22 16:48:09 -0700

    Add autocomplete and snippet packages

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -104,6 +104,11 @@ _G.packer_plugins = {
     path = "/Users/mcartledge/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["telescope-fzf-native.nvim"] = {
+    loaded = true,
+    path = "/Users/mcartledge/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
+    url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
+  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/Users/mcartledge/.local/share/nvim/site/pack/packer/start/telescope.nvim",
